5f27e32019a8136bfa5816d673cbb5e77ba70cbf
Logan Duvick <loganduvick@gmail.com>
2024-01-23 16:38:44 -0500

    updated

--- PowerTables/PowerTables.py
+++ PowerTables/PowerTables.py
@@ -1,15 +1,17 @@
-userInput = int(input("Enter an integer: "))
+confirmInput="y"
+while confirmInput=="y":
+    userInput = int(input("Enter an integer: "))
+    print("{:<10} {:<10} {:<10}".format("Number", "Squared", "Cubed"))
+    print("{:<10} {:<10} {:<10}".format("======","======","======"))
+    #print(str(userInput)+"         "+str(userInput*userInput)+"         "+str(userInput**3))
 
-print("{:<10} {:<10} {:<10}".format("Number", "Squared", "Cubed"))
-print("{:<10} {:<10} {:<10}".format("======","======","======"))
-#print(str(userInput)+"         "+str(userInput*userInput)+"         "+str(userInput**3))
+    for i in range (1, userInput+1):
+        print("{:<10} {:<10} {:<10}".format(i, i**2, i**3))
+        #print(str(userInput-i)+"         "+str(((userInput-i)*(userInput-i)))+"         "+str((userInput-i)**3))
 
-for i in range (1, userInput+1):
-    print("{:<10} {:<10} {:<10}".format(i, i**2, i**3))
-    #print(str(userInput-i)+"         "+str(((userInput-i)*(userInput-i)))+"         "+str((userInput-i)**3))
-
-print("\nMultiplication Table")
-print("{:<10} {:<10} {:<10} {:<10}".format(1,2,3,4))
-print("{:<10} {:<10} {:<10} {:<10}".format("=","=","=","="))
-for i in range (1, userInput+1):
-    print("{:<10} {:<10} {:<10} {:<10}".format(i, i * 2, i * 3, i * 4))
+    print("\nMultiplication Table")
+    print("{:<10} {:<10} {:<10} {:<10}".format(1,2,3,4))
+    print("{:<10} {:<10} {:<10} {:<10}".format("=","=","=","="))
+    for i in range (1, userInput+1):
+        print("{:<10} {:<10} {:<10} {:<10}".format(i, i * 2, i * 3, i * 4))
+    confirmInput = input("Do you want to continue? (y/n): ")
